7c8d1ef151456cb2d6d5b5cea7516c68da3fba9d
Rujia Dai <dair@upstate.edu>
2020-07-06 21:32:04 -0400

    Delete prs.bash

--- prs.bash
+++
@@ -1,2 +0,0 @@
-PRSice_linux --base base/25056061.2014Nature.sczVScontrol.base --target merge2.WGS-PsychChip-affy.missingLT04.maf001.recode.all --out PRS_imputedGenotypes.25056061.2014Nature.sczVScontrol --no-regress --bar-levels 5e-8,5e-7,5e-6,5e-5,5e-4,5e-3,5e-2,5e-1,1 –fastscore
-
